ca0f88c796f448b8c95fb2f09dfa5b0a218fdc03
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-21 11:15:48 +1300

    adds actors films to actor view

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,6 +2,10 @@ class Film < ActiveRecord::Base
   self.table_name = 'film'
   before_save :set_last_updated_at
 
+  def to_s
+    [release_year, title.titleize].join(' ')
+  end
+
   private
 
   def set_last_updated_at
