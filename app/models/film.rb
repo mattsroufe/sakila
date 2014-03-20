a13e1e92501129c50d93342844fa01c446b29e0c
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-21 12:10:30 +1300

    adds actors method to film

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,6 +1,8 @@
 class Film < ActiveRecord::Base
   self.table_name = 'film'
   before_save :set_last_updated_at
+  has_many :film_actors
+  has_many :actors, :through => :film_actors
 
   def to_s
     [release_year, title.titleize].join(' ')
--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe Film do
-  let(:film) { FactoryGirl.build(:film) }
+  let!(:film) { FactoryGirl.create(:film) }
 
   it "is valid with a title and language_id" do
     expect(film).to be_valid
+  end
+
+  describe "#actors" do
+    let!(:actor) { FactoryGirl.create(:actor) }
+
+    it "returns the film's actors" do
+      film.actors << actor
+      expect(film.actors).to match_array([actor])
+    end
   end
 end
