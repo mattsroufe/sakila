c3d0e80ba9da90576c0fef95cc1b44cea107524d
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-21 15:30:49 +1300

    adds Category#films and spec

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   self.table_name = 'category'
+  has_many :film_categories
+  has_many :films, :through => :film_categories
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,16 @@ require 'spec_helper'
 describe Category do
   let(:category) { FactoryGirl.create(:category) }
 
-  it "is valid" do
+  it "is valid with a name" do
     expect(category).to be_valid
+  end
+
+  describe "#films" do
+    let!(:film) { FactoryGirl.create(:film) }
+
+    it "returns the films within the category" do
+      category.films << film
+      expect(category.films).to match_array([film])
+    end
   end
 end
