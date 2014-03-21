b662a58de569baa5e3ee5914265f97c664cef50f
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-22 12:00:23 +1300

    adds Address#country

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Address do
   let!(:address) { FactoryGirl.create(:address) }
   let!(:city) { FactoryGirl.create(:city) }
+  let!(:country) { FactoryGirl.create(:country) }
 
   it "is valid with an address, district, city_id and phone" do
     expect(address).to be_valid
@@ -12,6 +13,13 @@ describe Address do
     it "returns the address' city" do
       address.city = city
       expect(address.city).to eq(city)
+    end
+  end
+
+  describe "#country" do
+    it "returns the address' country" do
+      address.country = country
+      expect(address.country).to eq(country)
     end
   end
 end
