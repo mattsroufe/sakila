6b6390a9a7accee5ae91b83c91f417eef823d60a
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-21 19:26:20 +1300

    adds Rental#customer and spec

--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -6,4 +6,13 @@ describe Rental do
   it "is valid with a rental date, customer_id, inventory_id, and staff_id" do
     expect(rental).to be_valid
   end
+
+  describe "#customer" do
+    let!(:customer) { FactoryGirl.create(:customer) }
+
+    it "returns the rental's customer" do
+      rental.customer = customer
+      expect(rental.customer).to eq(customer)
+    end
+  end
 end
