7f4348d30a51a1dcef41aee71cc0429151753477
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-21 19:32:16 +1300

    adds Customer#full_name and spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,10 @@ describe Customer do
   it "is valid store_id, first_name, last_name and address_id" do
     expect(customer).to be_valid
   end
+
+  describe "#full_name" do
+    it "returns the customer's full name" do
+      expect(customer.full_name).to eq("Matt Sroufe")
+    end
+  end
 end
