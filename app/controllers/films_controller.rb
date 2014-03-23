c6c719dae63b0d7d1ed213d1cc1e36eec6dfb7ec
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-23 21:21:39 +1300

    updates films index view

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -3,9 +3,9 @@ class FilmsController < ApplicationController
   def index
     if params[:category_id]
       @category = Category.find(params[:category_id])
-      @films = @category.films.filter_by('title', params[:letter]).page(params[:page])
+      @films = @category.films.filter_by('title', params[:letter]).page(params[:page]).per(5)
     else
-      @films = Film.filter_by('title', params[:letter]).page(params[:page])
+      @films = Film.filter_by('title', params[:letter]).page(params[:page]).per(5)
     end
   end
 
