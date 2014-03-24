b9d82d817a4ddc7b4280aefaf8856dadd880000f
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-03-24 20:49:40 +1300

    adds Payment#customer

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 
 describe Payment do
   let!(:payment) { FactoryGirl.create(:payment) }
+  let!(:customer) { FactoryGirl.create(:customer) }
 
   it "is valid customer_id, staff_id, amount and date" do
     expect(payment).to be_valid
+  end
+
+  describe "#customer" do
+    it "returns the payment's customer" do
+      expect(payment.customer).to eq(customer)
+    end
   end
 end
